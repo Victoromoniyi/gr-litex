25e23a20b77a4a8f8d159f39bcd75e022f2ba7c8
victoromoniyi <omoniyiv@cardiff.ac.uk>
2021-09-13 21:18:47 +0100

    added printing

--- gr-litexgnu/lib/litexgnu_impl.cc
+++ gr-litexgnu/lib/litexgnu_impl.cc
@@ -4,9 +4,35 @@
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
-
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/poll.h>
+#include <time.h>
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <gnuradio/io_signature.h>
 #include "litexgnu_impl.h"
+
+#include "litepcie.h"
+#include "config.h"
+#include "csr.h"
+#include "flags.h"
+
+#include "liblitepcie.h"
+
+static char litepcie_device[1024];
+static int litepcie_device_num;
 
 namespace gr {
   namespace litexgnu {
@@ -19,6 +45,23 @@ namespace gr {
         );
     }
 
+
+static void info(void)
+{
+    int fd;
+    int i;
+    unsigned char fpga_identification[256];
+
+    fd = open(litepcie_device, O_RDWR);
+    if (fd < 0) {
+        fprintf(stderr, "Could not init driver\n");
+        exit(1);
+    }
+
+    for(i=0; i<256; i++)
+        fpga_identification[i] = litepcie_readl(fd, CSR_IDENTIFIER_MEM_BASE + 4*i);
+    printf("FPGA identification: %s\n", fpga_identification);
+}
 
     /*
      * The private constructor
@@ -54,7 +97,13 @@ namespace gr {
       for(int i = 0; i < noutput_items; i++) {
         out[i] = in[i] * in[i];
       }
-      
+
+     const char *cmd;
+     litepcie_device_num = 0;
+
+     snprintf(litepcie_device, sizeof(litepcie_device), "/dev/litepcie%d", litepcie_device_num);
+    
+     info();     
 
       // Do <+signal processing+>
       // Tell runtime system how many input items we consumed on
